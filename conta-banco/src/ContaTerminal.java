fbd9435b861e63b856455ed764b5255fe17f250e
Vamblack-DMV <vamblack2000@gmail.com>
2023-10-26 10:01:21 -0300

    Finalizado com a correção de erro

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -6,26 +6,25 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        //TODO: Conhecer e importar a classe Scannwr: OK!
-        //Exibir as mensagens para o nosso usuário: OK!
-        //Obter pela scanner os valores digitados no terminal
-        //Exibir a mensagem conta criada
 
-        Scanner scanner =new Scanner(System.in).useLocale(Locale.US);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite o número da sua Agência:");
+        System.out.println("Digite o número e dígito da sua Agência:");
         String agencia = scanner.next();
 
         System.out.println("Digite o número da sua Conta:");
         int conta = scanner.nextInt();
 
-        System.out.println("Digite o seu nome completo:");
+        System.out.println("Digite o seu Nome:");
         String nome = scanner.next();
+
+        System.out.println("Digite o seu Sobrenome:");
+        String sobrenome = scanner.next();
 
         System.out.println("Digite o saldo de depósito:");
         double saldo = scanner.nextDouble();
 
-        System.out.println("Bom dia " + nome + ", obrigado por criar uma conta em nosso banco, sua Agência é " + agencia + ", conta " + conta + ", e seu saldo de" + saldo + ", já está disponível para saque.");
+        System.out.println("Bom dia " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua Agência é " + agencia + ", conta " + conta + ", e seu saldo de R$" + saldo + ", já está disponível para saque.");
 
     }
 }
