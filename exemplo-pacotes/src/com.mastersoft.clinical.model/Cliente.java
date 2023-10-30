a00a6ad935ba7eba7050247fe2053d62ea342a74
Vamblack-DMV <vamblack2000@gmail.com>
2023-10-30 15:58:15 -0300

    Exemplos de Pacotes

---
+++ exemplo-pacotes/src/com.mastersoft.clinical.model/Cliente.java
@@ -0,0 +1,8 @@
+package com.mastersoft.clinical.model;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ exemplo-pacotes/src/com.mastersoft.clinical.model/Exame.java
@@ -0,0 +1,8 @@
+package com.mastersoft.clinical.model;
+
+public class Exame {
+    
+    public static void main(String[] args) {
+        
+    }
+}
