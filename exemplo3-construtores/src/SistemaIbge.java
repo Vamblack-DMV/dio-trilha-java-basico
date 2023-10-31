b93ef167c94c4b1159f9446e347ea310223c97e8
Vamblack-DMV <vamblack2000@gmail.com>
2023-10-31 11:40:15 -0300

    Enum

---
+++ exemplo3-construtores/src/SistemaIbge.java
@@ -0,0 +1,20 @@
+public class SistemaIbge {
+    
+    public static void main(String[] args) {
+        //imprimindo os estados existentes no enum
+        for (EstadoBrasileiro uf: EstadoBrasileiro.values() ) {
+            System.out.println(uf.getSigla() + " - " + uf.getNomeMaiusculo());
+        }
+
+        //selecionando um estado a partir das opções disponíveis
+        EstadoBrasileiro ufSelecionado = EstadoBrasileiro.CEARA;
+
+        System.out.println("O Estado selecinoado foi: " + ufSelecionado.getNome());
+
+        System.out.println(ufSelecionado.getNome());        
+        System.out.println(ufSelecionado.getSigla());        
+        System.out.println(ufSelecionado.getNomeMaiusculo());        
+        System.out.println(ufSelecionado.getIbge());        
+
+    }
+}
