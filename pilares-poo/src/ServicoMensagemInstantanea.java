b8c2ee21d45cc62854d923222f1452f9f4abc616
Vamblack-DMV <vamblack2000@gmail.com>
2023-10-31 15:22:04 -0300

    abstação

--- pilares-poo/src/ServicoMensagemInstantanea.java
+++ pilares-poo/src/ServicoMensagemInstantanea.java
@@ -1,24 +1,5 @@
-public class ServicoMensagemInstantanea {
-    public void enviarMensagem () {
-        
-        //primeiro confirma se está conectado a internet
-        validarConectadoInternet ();
-        System.out.println("Enviando Mensagem");
-
-        //depois de enviada, salva o histórico da mensagem
-        salvarHistoricoMensagem ();
-    }
-
-    public void receberMensagem () {
-        System.out.println("Recebendo Mensagem");
-    }
-
-    //métodos privados, visíveis somente na classe
-    private void validarConectadoInternet () {
-        System.err.println("Validando se está conectado na internet");
-    }
-
-    private void salvarHistoricoMensagem () {
-        System.out.println("Salvando o histórico da mensagem");
-    }
+public abstract class ServicoMensagemInstantanea {
+    
+     public abstract void enviarMensagem ();
+     public abstract void receberMensagem ();
 }
--- pilares-poo/src/TelegramMessenger.java
+++ pilares-poo/src/TelegramMessenger.java
@@ -1,3 +1,11 @@
 public class TelegramMessenger extends ServicoMensagemInstantanea {
 
+    public void enviarMensagem () {
+        System.out.println("Enviando mensagem pelo Telegram");
+    }
+
+    public void receberMensagem () {
+        System.out.println("Recebendo mensagem pelo Telegram");
+    }
+
 }
